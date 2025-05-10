c2f26a9720e6b846a4cbc4d027b5f8d001360ef9
farhaan337 <farhaanmd2004@gmail.com>
2025-05-10 15:31:40 +0530

    Update ProductsSection.tsx
    Signed-off-by: farhaan337 <farhaanmd2004@gmail.com>

--- src/components/ProductsSection.tsx
+++ src/components/ProductsSection.tsx
@@ -68,7 +68,7 @@ const productData: ProductProps[] = [
     id: '8',
     name: 'Heart Resin',
     price: 99,
-    image: '/lovable-uploads/WhatsApp Image 2025-05-04 at 16.44.12_6f6ccbab.jpg',
+    image: 'public/lovable-uploads/WhatsApp_Image_2025-02-01_at_3.06.38_PM.jpeg',
     category: 'Home Decor',
     description: 'Personalized Heart Resin with your initial and favorite color'
   }
