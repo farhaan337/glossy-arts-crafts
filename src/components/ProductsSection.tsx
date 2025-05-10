56cf8dc61802e1198379f35048f45b6607b037a7
farhaan337 <farhaanmd2004@gmail.com>
2025-05-10 15:13:34 +0530

    Update ProductsSection.tsx
    Signed-off-by: farhaan337 <farhaanmd2004@gmail.com>

--- src/components/ProductsSection.tsx
+++ src/components/ProductsSection.tsx
@@ -45,7 +45,7 @@ const productData: ProductProps[] = [
     id: '5',
     name: 'Custom Text Keychain',
     price: 139,
-    image: '/lovable-uploads/WhatsApp Image 2025-05-04 at 16.44.15_38b906c9.jpg',
+    image: '/lovable-uploads/WhatsApp Image 2025-05-04 at 16.44.15_165f8c05.jpg',
     category: 'Keychains',
     description: 'Personalized keychain with your choice of text and color'
   },
